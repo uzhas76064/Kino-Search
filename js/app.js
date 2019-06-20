52d7135a1d8a92c3bceabf1b9f6052dc20156fcf
Maksim Hvatov <uzhas76064@gmail.com>
2019-06-20 12:41:21 +0300

    Rewritten app.js and added broken image styles

---
+++ js/app.js
@@ -0,0 +1,60 @@
+'use strict';
+window.addEventListener('DOMContentLoaded', () => {
+    let searchForm = document.querySelector('#search-form'),
+        movies = document.querySelector('.movies'),
+        posterImg = movies.querySelector('.poster');
+
+    searchForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const searchText = document.querySelector('#form-control').value,
+            apiKey = '08e954c9c496514c2ec00002ea60edfc',
+            language = 'en-US',
+            posterSize = 'w500',
+            server = `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=${language}&page=1&include_adult=false&query=${searchText}`;
+
+        fetch(server)
+            .then(value => {
+                if (value.status !== 200) {
+                    return Promise.reject(value);
+                }
+
+                return value.json();
+            })
+            .then(output => {
+                let inner = '';
+                let poster = `https://image.tmdb.org/t/p/${posterSize}/`;
+
+                //console.log(output);
+
+                output.results.forEach((item) => {
+                    let nameItem = item.name || item.title,
+                        firstSeries = item.first_air_date || item.release_date;
+
+                    item.poster_path !== null
+                        inner +=
+                        `<div class="movie-item">
+                            <img src="${poster + item.poster_path}" class="poster" alt="${nameItem}">
+                            <div class="info">
+                                <h3>${nameItem}</h3>
+                                <p>Release date: ${firstSeries}</p>
+                            </div>
+                         </div>`;
+                });
+
+                movies.innerHTML = inner;
+            })
+             .catch(err => {
+                movies.textContent += 'Oops.. Something went wrong';
+                console.log(err.status);
+            });
+    });
+
+    //Почему-то этот код не работает и я не могу понять почему
+    // поэтому стилизовал битое изображение в css
+    //Можно объяснить этот момент?
+
+    /* posterImg.addEventListener('error', () => {
+        posterImg.setAttribute('src', 'icon.png');
+    }); */
+
+});
--- js/promise.js
+++ js/promise.js
@@ -9,28 +9,18 @@ window.addEventListener('DOMContentLoaded', ()=> {
             apiKey = '08e954c9c496514c2ec00002ea60edfc',
             language = 'en-US',
             server = `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=${language}&page=1&include_adult=false&query=${searchText}`;
-        requestApi('GET', server);
-    });
 
-    function requestApi(method, url) {
-        let xhr = new XMLHttpRequest();
-        xhr.open(method, url);
-        xhr.send(null);
+        requestApi('GET', server)
+            .then(result => {
+                const output = JSON.parse(result);
+                let inner = '';
 
-        xhr.addEventListener('readystatechange', ()=> {
-            if(xhr.readyState !== 4) return;
+                console.log(output);
 
-            if(xhr.status !== 200) {
-                movies.textContent += xhr.status;
-                return;
-            }
-
-            const output = JSON.parse(xhr.responseText);
-            console.log(output);
-            let inner = '';
-            output.results.forEach((item)=> {
+                output.results.forEach((item)=> {
                 let nameItem = item.name || item.title,
                     firstSeries = item.first_air_date || item.release_date;
+
                 inner += `<div class="movie-item">
                             <h3>${nameItem}</h3>
                             <p>Release date: ${firstSeries}</p>
@@ -38,6 +28,30 @@ window.addEventListener('DOMContentLoaded', ()=> {
             });
 
             movies.innerHTML = inner;
+            })
+            .catch(err => {
+                movies.textContent += 'Oops.. Something went wrong';
+                console.log(err.status);
+            });
+    });
+
+    function requestApi(method, url) {
+        return new Promise((resolve, reject)=> {
+            let xhr = new XMLHttpRequest();
+            xhr.open(method, url);
+            xhr.addEventListener('load', ()=> {
+                if(xhr.status !== 200) {
+                    reject({status: xhr.status});
+                    return;
+                }
+
+                resolve(xhr.response);
+            });
+
+            xhr.addEventListener('error', ()=> {
+                reject({status: xhr.status});
+            });
+            xhr.send(null);
         });
     }
 });
